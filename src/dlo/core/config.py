5c9361846199c7b4ce7f3c68cb86abec2ba42def
JNPN7 <jpphanju54@gmail.com>
2026-03-29 02:35:17 +0530

    config init for profile, connections and engine

--- src/dlo/core/config.py
+++ src/dlo/core/config.py
@@ -1,6 +1,25 @@
+from typing import Dict, List
+
 from dlo.common.schema import SchemaBase
 
 
 class Project(SchemaBase):
     project_name: str
+    version: str
     project_root: str
+    profile: str
+    memory: List[str]
+
+
+class Engine(SchemaBase):
+    type: str
+    config: dict
+
+
+class Connection(SchemaBase):
+    ...
+
+
+class Profile(SchemaBase):
+    engine: Engine
+    connections: Dict[str, Connection]
